005b4c58ff5e9d991022c624dff46d44fe34a0b8
jack <mjay8558@gmail.com>
2023-05-29 21:35:07 +0900

    asdfsda

---
+++ 1package.R
@@ -0,0 +1,15 @@
+library('tidyverse');
+library("mirt")
+library("caret")
+library("psych")
+library("lavaan")
+library("vcd") # kappa
+library('ggplot2')
+library('cowplot') # plot_grid
+library('gridExtra') #grid.arrange
+library('grid')
+library("lemon")
+library("lattice");bwtheme <- standard.theme("pdf", color=FALSE) #mirt그림
+library('latticeExtra') #mirt그림
+library('ggrepel')
+select = dplyr::select
