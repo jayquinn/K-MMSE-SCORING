ddf5de33d7ba8c85dc45bcae686ce5346e752142
jack <mjay8558@gmail.com>
2023-06-08 08:47:35 +0900

    asdfasf

--- 2scoring.R
+++ 2scoring.R
@@ -1,8 +1,10 @@
 #23/24
-#score.SUM %>% quantile(probs = c(0.294944247))
+#score.SUM %>%quantile(probs = c(0.294944247))
 cutoff = 0.294944247
 dat<-read.csv("C:/git/K-MMSE-SCORING/mmse.csv",header=T,sep=",")
-attach(dat) #코드 정리시작
+# C420은 도움받은여부
+# str, w는 69~~이고 lt는 61인데 str, w가 다 포함함. 아마 lt는 추가 안된 오리지날 같고... 어쨋든 데이터 동일함.
+attach(dat) #코드 전처리 시작
 #401 시간지남력 - 연월일 0123 
 a401<-ifelse(C401==3,3,
              ifelse(C401==2,2,
@@ -99,7 +101,8 @@ a416<-ifelse(C416==3,3,
                            ifelse(C416==-8,NA,
                                   ifelse(C416==-9,0,
                                          ifelse(C416==5,0,NA))))))
-#417 명령시행_읽고 눈감기 01 #코드상 읽기 1, 눈감기 3으로 되어있는데 원전상 눈을 감아야만 점수 획득임.
+#417 명령시행_읽고 눈감기 01 
+#코드상 읽기 1, 눈감기 3으로 되어있는데 원전상 눈을 감아야만 점수 획득임.
 a417<-ifelse(C417==3,1,
              ifelse(C417==1,0,
                     ifelse(C417==-8,NA,
@@ -115,7 +118,7 @@ a419<-ifelse(C419==1,1,
              ifelse(C419==-8,NA,
                     ifelse(C419==-9,0,
                            ifelse(C419==5,0,NA))))
-#420 도움받은정도 -9 모르겠음 -8 응답거부 1도움전혀받지않음 2가끔 3대부분
+#420 도움받은정도 -9 모르겟음 -8 응답거부 1도움전혀받지않음 2가끔 3대부분
 a420<-ifelse(C420==3,3,
              ifelse(C420==2,2,
                     ifelse(C420==1,1,
@@ -123,10 +126,11 @@ a420<-ifelse(C420==3,3,
                                   ifelse(C420==-9,NA,NA)))))
 #취합
 response<-data.frame(a401,a402,a403,a404,a405,a406,a407,a408,a409,a410,a411,a412,a413,a414,a415,a416,a417,a418,a419)
+#response<-data.frame(a401,a402,a403,a404,a405,a406,a407,a408,a409,a410,a411)
 #인지기능 저하자 마킹
 response$diag<-ifelse(dat$diag==5,0,
                       ifelse(dat$diag==3,1,
-                             ifelse(dat$diag==1,1,dat$diag))) #5 치매아님 -9 모르겠음 -8 응답거부 1 치매 3 경도인지장애
+                             ifelse(dat$diag==1,1,dat$diag))) #5 치매아님 -9 모르겟음 -8 응답거부 1 치매 3 경도인지장애
 #나이 입력
 response$age = 2018-dat$year
 response$gender = dat$gender # 1 = 남자, 5 = 여자
@@ -134,9 +138,9 @@ response$gender = dat$gender # 1 = 남자, 5 = 여자
 response = response %>% filter(complete.cases(.))
 detach(dat)
 colSums(is.na(response))
-#19문항
+#19문항짜리
 response19<-response
-#5문항
+#5문항짜리
 response05<-response %>% mutate(a401 = a401+a402+a403+a404+a405,
                                 a402 = a406,
                                 a403 = a407+a408+a409+a410+a411,
@@ -144,15 +148,16 @@ response05<-response %>% mutate(a401 = a401+a402+a403+a404+a405,
                                 a405 = a413+a414+a415+a416+a417+a418+a419,
                                 .keep = "unused")
 ############################
-#####점수산출시작###########
+#####분석시작###############
 ############################
 
-#SUM score
+#SUMmed score
 score.SUM<-vector("double",nrow(response05))
 for ( i in 1:nrow(response05) ){
   score.SUM[[i]]<-sum(response05[i,1:5],na.rm=T)}
 
 #CFA factor score
+#CFA 점수 산출
 model.cfa<-'F1 =~ a401 + a402 + a403 + a404 + a405'
 results.cfa<-cfa(model=model.cfa,data = response05[,1:5],
                  estimator = "MLM")
@@ -179,7 +184,6 @@ M2(results.gpcm)
 #점수 취합
 score.frame<-cbind(score.SUM,score.CFA,score.PCM,score.GPCM,response$diag,response$age,response$gender); colnames(score.frame)<-c("SUM","CFA","PCM","GPCM","diag","age","gender")
 as.data.frame(score.frame) -> score.frame
-
 #23/24기준에 마커
 score.frame %>% mutate(markerSUM = case_when(SUM <= quantile(score.frame$SUM,cutoff) ~ '1',
                                              SUM > quantile(score.frame$SUM,cutoff) ~ '0'),
@@ -190,11 +194,10 @@ score.frame %>% mutate(markerSUM = case_when(SUM <= quantile(score.frame$SUM,cut
                        markerGPCM = case_when(GPCM <= quantile(score.frame$GPCM,cutoff) ~ '1',
                                               GPCM > quantile(score.frame$GPCM,cutoff) ~ '0')) -> sf
 
-sf %>% mutate(agegroup = case_when(age >= 80 ~ '4',
-                                   age >= 70 & age < 80 ~ '3',
-                                   age >= 60 & age < 70 ~ '2',
-                                   age < 60 ~ '1')) -> sf
+sf %>% mutate(agegroup = case_when(age >= 85 ~ '4',
+                                   age >= 75 & age < 85 ~ '3',
+                                   age >= 65 & age < 75 ~ '2',
+                                   age >= 55 & age < 65 ~ '1')) -> sf
 sf <- mutate_at(sf, vars(starts_with("marker")), as.factor)
 sf$diag <- as.factor(sf$diag)
 sf$gender <- as.factor(sf$gender)
-
